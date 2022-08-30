15e0a23fb1200d474927128583d86a481679141d
JJMarin-08345 <110709879+JJMarin-08345@users.noreply.github.com>
2022-08-30 10:19:09 -0500

    Delete Entidades directory

--- Entidades/Escenario.cs
+++
@@ -1,9 +0,0 @@
-namespace Dominio{
-    public class Escenario{
-        public int Id {get;set;}
-        public string Nombre {get;set;}
-        public int Espectadores {get;set;}
-        public string Tipo {get;set;}
-        public int UnidadDeportivaId {get;set;}
-    }
-}
--- Entidades/TorneoEquipo.cs
+++
@@ -1,9 +0,0 @@
-namespace Dominio{
-    public class TorneoEquipo{
-        public int Id {get;set;}
-        public int EquipoId {get;set;}
-        public int TorneoId {get;set;}
-        public Torneo Torneo {get;set;}
-        public Equipo Equipo {get;set;}
-    }
-}

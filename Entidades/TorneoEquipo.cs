7e9055185f3ade9bb022b72dda3283516c1821b0
JJMarin-08345 <110709879+JJMarin-08345@users.noreply.github.com>
2022-09-01 15:19:13 -0500

    Update TorneoEquipo.cs

--- Entidades/TorneoEquipo.cs
+++ Entidades/TorneoEquipo.cs
@@ -5,10 +5,11 @@ using System.Collections.Generic;
 
 namespace Dominio{
     public class TorneoEquipo{
-        public int Id {get;set;}
-        public int EquipoId {get;set;}
-        public int TorneoId {get;set;}
-        public Torneo Torneo {get;set;}
+        //public int Id {get;set;}
+        public int? EquipoId {get;set;}
         public Equipo Equipo {get;set;}
+        public int? TorneoId {get;set;} 
+        public Torneo Torneo {get;set;}
+        //El ? es para decir que pueden tomar el valor null
     }
 }

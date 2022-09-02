df1c7d7a1858b84647cd3377a3a6d5febb847b0c
diegoaba1ben <108026491+diegoaba1ben@users.noreply.github.com>
2022-09-02 10:28:24 -0500

    Update TorneoEquipo.cs

--- TorneoEquipo.cs
+++ TorneoEquipo.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 namespace Dominio{
     public class TorneoEquipo{
+        
         public int Id {get;set;}
         public int? EquipoId {get;set;}
         public Equipo Equipo {get;set;}

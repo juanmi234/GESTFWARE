08bb76c7650f133a83397bef5c0225a2c2519238
diegoaba1ben <108026491+diegoaba1ben@users.noreply.github.com>
2022-09-05 12:22:24 -0500

    Add files via upload

--- Entidades/Colegios.cs
+++ Entidades/Colegios.cs
@@ -4,14 +4,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 
 namespace Dominio{
-    public class Arbitro{
+    public class Colegios{
         public int Id {get;set;}
 
         [Required(ErrorMessage="Este campo es obligatorio")]
         [MaxLength(15, ErrorMessage="El campo {0} debe ser máximo de {1} caracteres")]
         [MinLength(5, ErrorMessage="El campo {0} debe tener minimo {1} caracteres")]
         [RegularExpression("([0-9]+)", ErrorMessage="El campo {0} solo acepta valores numericos")]
-        public string Documento {get;set;}
+        public string NIT {get;set;}
 
         [Required(ErrorMessage="Este campo es obligatorio")]
         [MaxLength(30, ErrorMessage="El campo {0} debe ser máximo de {1} caracteres")]
@@ -42,11 +42,6 @@ namespace Dominio{
         [RegularExpression("([0-9]+)", ErrorMessage="El campo {0} solo acepta valores numericos")]
         public string Celular {get;set;}
 
-
-        public int ColegioId {get;set;}
-
-
-        public int TorneoId {get;set;}
-
+        public List<Arbitro> Arbitros { get; set; }
     }
 }
--- Entidades/TorneoEquipo.cs
+++ Entidades/TorneoEquipo.cs
@@ -4,8 +4,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 
 namespace Dominio{
+
+    
     public class TorneoEquipo{
-        //public int Id {get;set;}
+
+        public int Id {get;set;}
         public int? EquipoId {get;set;}
         public Equipo Equipo {get;set;}
         public int? TorneoId {get;set;} 

5149cfd2ae60effdf6ce7202fac7a58e5d565740
JJMarin-08345 <110709879+JJMarin-08345@users.noreply.github.com>
2022-09-06 09:11:58 -0500

    Add files via upload

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -1,8 +1,31 @@
-﻿namespace Consola{
+﻿using Dominio;
+using Persistencia;
+using System;
+using System.Collections.Generic;
+
+namespace Consola{
     class Program{
+        private static int op=0;
+        private static IRMunicipio objMunicipio = new RMunicipio(new Persistencia.AppContext());
         static void Main(string[] args){
-            Console.WriteLine("Hello, World!");
+
+            //Console.WriteLine("hOLA Maskdaj");
+            Municipio municipio = new Municipio();
+            municipio.Nombre="Medellin";
+            bool ex=objMunicipio.CrearMunicipio(municipio);
+            //var mun=objMunicipio.EliminarMunicipio(1);
+            //Console.WriteLine(municipio.Id+" "+municipio.Nombre);
+
+            if(ex){
+                Console.WriteLine("Creado el municipio {0}",municipio.Nombre);
+            }else{
+                Console.WriteLine("No Creado");
+            }
+            //Console.WriteLine("Desea borrar el Municipio {0}?",muni.Nombre);
+            //op=int.Parse(Console.ReadLine());
+            //if(op==1){
+              //  objMunicipio.EliminarMunicipio(muni.Id);
+            //}
         }
     }
 }
-

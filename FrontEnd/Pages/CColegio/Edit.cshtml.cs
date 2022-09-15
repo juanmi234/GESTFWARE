9defd8951c6e0918784314c5e2a675f048313a73
JJMarin-08345 <110709879+JJMarin-08345@users.noreply.github.com>
2022-09-15 12:10:01 -0500

    Add files via upload

--- FrontEnd/Pages/CColegio/Edit.cshtml.cs
+++ FrontEnd/Pages/CColegio/Edit.cshtml.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Persistencia;
+using Dominio;
 
 namespace FrontEnd.Pages.CColegio
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
-        {
+        //Atributos
+        private readonly IRColegioInstruccion objColegio;
+        [BindProperty]
+        public ColegioInstruccion colegio {get;set;}
+        //Metodos 
+        //Constructor por defecto
+        public EditModel(IRColegioInstruccion _objColegio){
+            this.objColegio = _objColegio;
+        }
+        public ActionResult OnGet(int id){
+            colegio = objColegio.BuscarColegio(id);
+            if(colegio!=null){
+                ViewData["Error"]="No se encontró el colegio";
+                return Page();
+            }
+            return Page();
+        }
+        public ActionResult OnPost(){
+            if(!ModelState.IsValid){
+                return Page();
+            }
+            if(objColegio.ModificarColegio(colegio)){
+                return RedirectToPage("./Index");
+            }else{
+                ViewData["Error"]="Ya existe un colegio con este mismo NIT";
+                return Page();
+            }
+            return Page();
         }
     }
 }

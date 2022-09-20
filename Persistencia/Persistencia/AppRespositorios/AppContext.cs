beaefdfb0cbb932df14c342fd3aa26bc36d3df98
JJMarin-08345 <110709879+JJMarin-08345@users.noreply.github.com>
2022-09-20 01:18:22 -0500

    Add files via upload

---
+++ Persistencia/Persistencia/AppRespositorios/AppContext.cs
@@ -0,0 +1,62 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Dominio;
+using System;
+using System.Collections.Generic;
+
+namespace Persistencia{
+    public class AppContext:DbContext{
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            //Todo torneo se relaciona con varios equipos, todo equipo se relaciona con varios torneos
+            //modelBuilder.Entity<Equipo>().HasMany(E=>E.Torneos).WithMany(E=>E.Equipos);
+            modelBuilder.Entity<Municipio>().HasIndex(m=>m.Nombre).IsUnique();
+            modelBuilder.Entity<Arbitro>().HasIndex(ar=>ar.Documento).IsUnique();
+            modelBuilder.Entity<ColegioInstruccion>().HasIndex(c=>c.Nit).IsUnique();
+            //modelBuilder.Entity<Torneo>().HasIndex(tor=>tor.Nombre).IsUnique();
+
+            modelBuilder.Entity<Patrocinador>()
+                        .HasMany(p=>p.Equipos)
+                        .WithOne(e=>e.patrocinador)
+                        .OnDelete(DeleteBehavior.Restrict);
+        }
+
+        //Conectar con la base de datos
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            if(!optionsBuilder.IsConfigured){
+                optionsBuilder.UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=BD_EscenarioDeportivo");
+            }
+        }
+
+        /*Todo esto de abajo se hace para que la base de datos cree
+        las tablas, ademas de servir para luego usarlas en los métodos CRUD
+        */
+        public DbSet<Municipio> Municipios {get;set;}
+        public DbSet<Torneo> Torneos {get;set;}
+        public DbSet<Equipo> Equipos {get;set;} 
+        public DbSet<TorneoEquipo> TorneoEquipos {get;set;}
+        public DbSet<Patrocinador> Patrocinadores {get;set;}
+        public DbSet<Arbitro> Arbitros {get;set;}
+        public DbSet<ColegioInstruccion> ColegiosInstrucciones {get;set;}
+        public DbSet<UnidadDeportiva> UnidadesDeportivas {get;set;}
+        public DbSet<Entrenador> Entrenadores {get;set;}
+        public DbSet<Deportista> Deportistas {get;set;}
+        public DbSet<Escenario> Escenarios {get;set;}
+        //Atajo teclado para abrir la terminal: Ctrl+Shift+Ñ
+
+        //Comando para crear la migracion, debe estar en el la carpeta de persistencia:
+        //dotnet ef add migrations ElNombreQueQuiera
+        //Comando para crear o actualizar la Base de Datos, debe estar en la carpeta persistencia:
+        //dotnet ef database update 
+
+        //Comando para verificar errores en las capas cualquiera:
+        //dotnet build
+
+        //Comando para crear la Consola:
+        //dotnet new console -o NombreQueLeQuieraPoner(usualmente es Consola)
+
+        //Comando para correr la consola:
+        //dotnet run 
+
+    }
+
+}
---
+++ Persistencia/Persistencia/AppRespositorios/IRDeportista.cs
@@ -0,0 +1,15 @@
+using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Persistencia{
+    public interface IRDeportista{
+        public bool CrearDeportista(Deportista deportista);
+        public Deportista BuscarDeportista(int id);
+        public bool ModificarDeportista(Deportista depor);
+        public bool EliminarDeportista(int id);
+        public List<Deportista> ListarDeportistaList();
+        public IEnumerable<Deportista> ListarDeportistaIE();
+    }
+}

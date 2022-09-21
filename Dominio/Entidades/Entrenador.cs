a2e4a5312d7162195df449d79fe52fff42700ace
JuanMBetancur <70607380+juanmi234@users.noreply.github.com>
2022-09-21 16:38:21 -0500

    Add files via upload

---
+++ Dominio/Entidades/Entrenador.cs
@@ -0,0 +1,49 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dominio{
+    public class Entrenador{
+        public int Id {get;set;}
+
+        [Required(ErrorMessage="Este campo es obligatorio")]
+        [MaxLength(15, ErrorMessage="El campo {0} debe ser máximo de {1} caracteres")]
+        [MinLength(5, ErrorMessage="El campo {0} debe tener minimo {1} caracteres")]
+        [RegularExpression("([0-9]*)", ErrorMessage="El campo {0} solo acepta valores numericos")]
+        public string Documento {get;set;}
+
+        [Required(ErrorMessage="Este campo es obligatorio")]
+        [MaxLength(30, ErrorMessage="El campo {0} debe ser máximo de {1} caracteres")]
+        [MinLength(5, ErrorMessage="El campo {0} debe tener minimo {1} caracteres")]
+        [RegularExpression("([A-Za-z ]*)", ErrorMessage="El campo {0} no acepta valores numericos")]
+        public string Nombres {get;set;}
+
+        [Required(ErrorMessage="Este campo es obligatorio")]
+        [MaxLength(40, ErrorMessage="El campo {0} debe ser máximo de {1} caracteres")]
+        [MinLength(5, ErrorMessage="El campo {0} debe tener minimo {1} caracteres")]
+        [RegularExpression("([A-Za-z ]*)", ErrorMessage="El campo {0} no acepta valores numericos")]
+        public string Apellidos {get;set;}
+
+        [Required(ErrorMessage="Este campo es obligatorio")]
+        [MaxLength(20, ErrorMessage="El campo {0} debe ser máximo de {1} caracteres")]
+        [MinLength(5, ErrorMessage="El campo {0} debe tener minimo {1} caracteres")]
+        [RegularExpression("([A-Za-z0-9 ]*)", ErrorMessage="Ingrese valores validos")]
+        public string Deporte {get;set;}
+
+        [Required(ErrorMessage="Este campo es obligatorio")]
+        [MaxLength(15, ErrorMessage="El campo {0} debe ser máximo de {1} caracteres")]
+        [MinLength(5, ErrorMessage="El campo {0} debe tener minimo {1} caracteres")]
+        [Range(3000000000,3509999999, ErrorMessage="El campo {0} solo acepta valores numericos")]
+        public string Celular {get;set;}
+
+        [Required(ErrorMessage="Este campo es obligatorio")]
+        [MaxLength(20, ErrorMessage="El campo {0} debe ser máximo de {1} caracteres")]
+        [MinLength(5, ErrorMessage="El campo {0} debe tener minimo {1} caracteres")]
+        public string Correo {get;set;}
+
+        //Llaves foraneas
+        [Required(ErrorMessage="Un entrenador debe tener asignado un equipo")]
+        public int EquipoId {get;set;}
+        public Equipo equipo {get;set;}//Propiedad navigacional
+    }
+}
